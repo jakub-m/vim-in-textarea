e9e74763e5047d404630af5560a7248e54be1e42
Jakub Mikians <jakub.mikians@gmail.com>
2012-12-29 15:55:34 +0100

    updated demo

--- demo/fiddle.js
+++ demo/fiddle.js
@@ -2,12 +2,20 @@ window.onload = function(){
   /* create VIM instance for each textarea */
   var vim = new VIM()
 
-  /* log to console log */
-  vim.on_log = function(x){ console.log('VIM: ' + x.toString()) }
+  /* log debug messages */
+  // vim.on_log = function(m){ console.log('VIM: '+m) }
+  vim.on_log = function(m) {
+    var LENGTH = 10
+    var p = $('<div></div>').text( m )
+    $('#log').prepend( p )
+    if ( $('#log').children().length > LENGTH ) {
+      $('#log').children(':last').remove()
+    }
+  }
 
   var target = document.getElementById('sandbox')
   if (target !== null) {
-    /* attach vim */
+    /* attach vim. this is the only line really needed */
     vim.attach_to( target )
     target.focus()
   }
